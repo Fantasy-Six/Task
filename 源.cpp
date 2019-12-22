d4beb0e03d2dd14db97ecc28934a2c42c3879805
Fantasy-Six <56625593+Fantasy-Six@users.noreply.github.com>
2019-12-22 23:34:49 +0800

    Add files via upload

---
+++ 源.cpp
@@ -0,0 +1,207 @@
+#include <iostream>  
+#include <OpenMesh/Core/IO/MeshIO.hh>  
+#include <OpenMesh/Core/Mesh/TriMesh_ArrayKernelT.hh>  
+#include "GL\glut.h"
+#include <math.h>
+#include <Windows.h>
+#include <string>
+
+using namespace std;
+typedef OpenMesh::TriMesh_ArrayKernelT<> MyMesh;
+
+//鼠标交互有关的
+int mousetate = 0; //鼠标当前的状态
+GLfloat Oldx = 0.0; // 点击之前的位置
+GLfloat Oldy = 0.0;
+//与实现角度大小相关的参数，只需要两个就可以完成
+float xRotate = 0.0f;
+float yRotate = 0.0f;
+float ty = 0.0f;
+float scale = 1;
+int flat = 0;
+int vertices = 0;
+
+//文件读取有关的
+MyMesh mesh;
+const string file_1 = "C:/Users/Fantasy-Six/source/repos/task1/pikaqiu.obj";
+int currentfile = 1;
+
+GLuint showFaceList, showWireList;
+int showstate = 1;
+bool showFace = true;
+bool showWire = false;
+bool showFlatlines = false;
+
+//初始化顶点和面   
+void initGL()
+{
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glClearDepth(2.0);
+	glShadeModel(GL_SMOOTH);
+	glEnable(GL_DEPTH_TEST); 
+	glEnable(GL_LIGHTING); 
+	glEnable(GL_LIGHT0); 
+	showFaceList = glGenLists(1);
+	showWireList = glGenLists(1);
+	int temp = mesh.n_edges();
+
+	// 绘制 wire 
+	glNewList(showWireList, GL_COMPILE);
+	glDisable(GL_LIGHTING);
+	glLineWidth(1.0f);
+	glColor3f(0.5f, 0.5f, 0.5f);
+	glBegin(GL_LINES);
+	for (MyMesh::HalfedgeIter he_it = mesh.halfedges_begin(); he_it != mesh.halfedges_end(); ++he_it) {
+		//链接这个有向边的起点和终点
+		glVertex3fv(mesh.point(mesh.from_vertex_handle(*he_it)).data());
+		glVertex3fv(mesh.point(mesh.to_vertex_handle(*he_it)).data());
+		vertices++;
+	}
+	glEnd();
+	glEnable(GL_LIGHTING);
+	glEndList();
+
+	// 绘制flat
+	glNewList(showFaceList, GL_COMPILE);
+	for (MyMesh::FaceIter f_it = mesh.faces_begin(); f_it != mesh.faces_end(); ++f_it) {
+		glBegin(GL_TRIANGLES);
+		for (MyMesh::FaceVertexIter fv_it = mesh.fv_iter(*f_it); fv_it.is_valid(); ++fv_it) {
+			glNormal3fv(mesh.normal(*fv_it).data());
+			glVertex3fv(mesh.point(*fv_it).data());
+		}
+		flat++;
+		glEnd();
+	}
+	glEndList();
+}
+
+// 当窗体改变大小的时候
+void myReshape(GLint w, GLint h)
+{
+	glViewport(0, 0, static_cast<GLsizei>(w), static_cast<GLsizei>(h));
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	if (w > h)
+		glOrtho(-static_cast<GLdouble>(w) / h, static_cast<GLdouble>(w) / h, -1.0, 1.0, -100.0, 100.0);
+	else
+		glOrtho(-1.0, 1.0, -static_cast<GLdouble>(h) / w, static_cast<GLdouble>(h) / w, -100.0, 100.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+
+// 读取文件的函数
+void readfile(string file) {
+	// 请求顶点法线 vertex normals
+	mesh.request_vertex_normals();
+	//如果不存在顶点法线，则报错 
+	if (!mesh.has_vertex_normals())
+	{
+		cout << "错误：标准定点属性 “法线”不存在" << endl;
+		return;
+	}
+	// 如果有顶点发现则读取文件 
+	OpenMesh::IO::Options opt;
+	if (!OpenMesh::IO::read_mesh(mesh, file, opt))
+	{
+		cout << "无法读取文件:" << file << endl;
+		return;
+	}
+	else cout << "成功读取文件:" << file << endl;
+	cout << endl; // 为了ui显示好看一些
+				  //如果不存在顶点法线，则计算出
+	if (!opt.check(OpenMesh::IO::Options::VertexNormal))
+	{
+		// 通过面法线计算顶点法线
+		mesh.request_face_normals();
+		// mesh计算出顶点法线
+		mesh.update_normals();
+		// 释放面法线
+		mesh.release_face_normals();
+	}
+}
+
+
+// 鼠标交互
+void myMouse(int button, int state, int x, int y)
+{
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+		mousetate = 1;
+		Oldx = x;
+		Oldy = y;
+	}
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_UP)
+		mousetate = 0;
+	//滚轮事件
+	if (state == GLUT_UP && button == 3) {
+		if (currentfile == 3)
+			scale -= 0.002;
+		else
+			scale -= 0.1;
+	}
+	if (state == GLUT_UP && button == 4) {
+		if (currentfile == 3)
+			scale += 0.002;
+		else
+			scale += 0.1;
+	}
+	glutPostRedisplay();
+}
+
+// 鼠标运动时
+void onMouseMove(int x, int y) {
+	if (mousetate) {
+		//x对应y是因为对应的是法向量
+		yRotate += x - Oldx;
+		glutPostRedisplay();
+		Oldx = x;
+		xRotate += y - Oldy;
+		glutPostRedisplay();
+		Oldy = y;
+	}
+}
+
+void myDisplay()
+{
+	//要清除之前的深度缓存
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
+	//与显示相关的函数
+	glRotatef(xRotate, 1.0f, 0.0f, 0.0f); // 让物体旋转的函数 第一个参数是角度大小，后面的参数是旋转的法向量
+	glRotatef(yRotate, 0.0f, 1.0f, 0.0f);
+	glTranslatef(0.0f, 0.0f, ty);
+	glScalef(scale, scale, scale); // 缩放
+
+								   //每次display都要使用glcalllist回调函数显示想显示的顶点列表
+	if (showFace)
+		glCallList(showFaceList);
+	if (showFlatlines) {
+		glCallList(showFaceList);
+		glCallList(showWireList);
+	}
+	if (showWire)
+		glCallList(showWireList);
+
+	glutSwapBuffers(); //这是Opengl中用于实现双缓存技术的一个重要函数
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH); // GLUT_Double 表示使用双缓存而非单缓存
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(800, 500);
+	glutCreateWindow("Mesh Viewer");
+	//一开始默认读取文件1
+	readfile(file_1);
+	initGL();
+	glutMouseFunc(myMouse);
+	glutMotionFunc(onMouseMove); // 鼠标移动的时候的函数
+	glutReshapeFunc(&myReshape);
+	glutDisplayFunc(&myDisplay);
+	std::cout << "顶点个数为：" << vertices << endl;
+	std::cout << "三角形个数为：" << flat << endl;
+	glutMainLoop();
+	return 0;
+}
+
